84fbd418621536a15e4865942e039fc60b4d23ab
vasilevdim <vasilevdim@gmail.com>
2017-04-02 17:54:42 +0300

    Modul08 Video02

---
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace IOTextFiles
+{
+	public class IOSettings
+	{
+
+		private STable _stable;
+
+
+		public IOSettings(STable stable)
+		{
+			_stable = stable;
+
+		}
+
+		public bool save()
+		{
+			try
+			{
+				string _temp = "";
+
+				_temp = String.Join("\r\n", _stable.stable);
+
+				//Zapis na tekstov fail
+
+				System.IO.File.WriteAllText("C:\\aula\\test.txt", _temp);
+
+				return true;
+			}
+			catch
+			{
+
+			}
+			return false;
+		}
+
+	}
+}
---
+++ IOTesting/IOTextFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IOTextFiles
+{
+	class MainClass
+	{
+		
+
+
+		public static void Main(string[] args)
+		{
+			STable _STable = new STable();
+			IOSettings _io = new IOSettings(_STable);
+
+			Console.WriteLine(_STable.stable [0]);
+
+			if (_io.save())
+			{
+				Console.WriteLine("Tablizata e zapametena uspeshno.");
+
+			}
+			else
+			{
+				Console.WriteLine("Tablizata NE e zapametena uspeshno.");
+			}
+		}
+	}
+}
