3651e02d02959673006f3f2fe860a6a317b32276
vasilevdim <vasilevdim@gmail.com>
2017-04-05 21:33:43 +0300

    Modul08 Video 06

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -15,15 +15,20 @@ namespace IOTextFiles
 		}
 
 		public string getPath()
+
+		//Programa\Settings\settings.txt
 		{
-			string _path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+			string _path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings");
+
+			_path = System.IO.Path.Combine(_path, "Settings.txt");
 
 			//Drugi vidove direktorii
 
-			string _user = Environment.GetFolderPath ( Environment.SpecialFolder.LocalApplicationData);
-			Console.WriteLine ( _user );
+			//string _user = Environment.GetFolderPath ( Environment.SpecialFolder.LocalApplicationData);
+			//Console.WriteLine ( _user );
 
-			string _desktop = Environment.GetFolderPath ( Environment.SpecialFolder.Desktop); Console.WriteLine (_desktop);
+			//string _desktop = Environment.GetFolderPath ( Environment.SpecialFolder.Desktop); 
+			//Console.WriteLine (_desktop);
 
 			return _path;
 		}
@@ -52,14 +57,30 @@ namespace IOTextFiles
 		{
 			try
 			{
-				string _temp = System.IO.File.ReadAllText (getPath ());
+				iniSettings();
+				
 
-				string[] _table = _temp.Replace("\r","").Split('\n');
+				string _temp = "", _filePath = getPath();
 
-				for (int i = 0; i < _table.Length; i++)
+				if (System.IO.File.Exists(_filePath))  //Proverka dali patiat e validen
 				{
-					_stable.stable[i] = _table[i];
+
+					System.IO.File.ReadAllText(_filePath);
+
+					string[] _table = _temp.Replace("\r", "").Split('\n');
+
+					for (int i = 0; i < _table.Length; i++)
+					{
+						_stable.stable[i] = _table[i];
+					}
 				}
+				else {
+					
+					return false;
+
+				}
+
+				System.Diagnostics.Process.Start( _filePath );
 
 				return true;
 			}
@@ -68,6 +89,34 @@ namespace IOTextFiles
 
 			}
 			return false;
+		}
+
+		private void iniSettings()
+		{
+			try
+			{
+				bool _filExists = System.IO.File.Exists ( getPath());
+
+				if (!_filExists)
+				{
+					string _directory = System.IO.Path.GetDirectoryName (getPath ());
+
+					//Zastrahovame se, che directoriata sashtestvuva
+
+					if (System.IO.Directory.Exists(_directory))
+					{
+						System.IO.Directory.CreateDirectory(_directory);
+					}
+
+					//Zapametiavame sadarjanieto na file-a
+				}
+				save();
+
+			}
+			catch { }
+
+
+
 		}
 	}
 }
