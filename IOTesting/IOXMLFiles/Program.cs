5c8512afb3874d3892ecc8c3c1ce7e41d799af5b
vasilevdim <vasilevdim@gmail.com>
2017-04-06 21:41:30 +0300

    Modul 08 Video07

---
+++ IOTesting/IOXMLFiles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace IOXMLFiles
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string value = "simple value", property = "simple property";
+
+			string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.xml");
+
+			using (XmlWriter writer = XmlWriter.Create(path))
+			{
+				writer.WriteStartDocument();
+
+				writer.WriteStartElement("Settings");
+
+				//File Content
+				writer.WriteStartElement("row");
+
+
+
+				writer.WriteAttributeString("property", property); //<row property = ".....">.....</row 
+				writer.WriteValue(value); //<row> value </row>
+
+				writer.WriteEndElement();
+
+				writer.WriteEndElement();
+
+				writer.WriteEndDocument();
+
+			}
+
+			XDocument document = XDocument.Load(path);
+			document.Save(path);
+
+			System.Diagnostics.Process.Start(path);
+		}
+	}
+}
